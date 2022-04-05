c3b04acbdcaa35cf31dbabb987d1b120df16aaeb
James Tang <jamestang@Jamess-MacBook-Pro.local>
2022-04-05 12:42:49 -0400

    adding feat/voting

--- poll/poll_project/poll/views.py
+++ poll/poll_project/poll/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-
+from django.http import HttpResponse
 
 from .forms import CreatePollForm
 from .models import Poll
@@ -26,7 +26,25 @@ def create(request):
     return render(request, 'poll/create.html', context)
 
 def vote(request, poll_id):
-    context = {}
+    poll = Poll.objects.get(pk=poll_id)
+
+    if request.method == 'POST':
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'Invalid form')
+
+        poll.save()
+        return redirect('results', poll.id)
+
+    context = {
+        'poll': poll
+        }
     return render(request, 'poll/vote.html', context)
 
 def results(request, poll_id):
